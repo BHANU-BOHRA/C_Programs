ca73ccc7eae80bea4b6e9deef0d29527a012130a
BHANU-BOHRA <120611061+BHANU-BOHRA@users.noreply.github.com>
2023-01-25 12:23:30 +0530

    Update Ackramann function.c

--- Ackramann function.c
+++ Ackramann function.c
@@ -1,15 +1,9 @@
 #include<stdio.h>
 int a(int m,int n)
 {
-	if(m==0)
-	{
-		return n+1;
-	}
-	else if(n==0)
-	{
-		return a(m-1,1);
-	}
-	else return a(m-1,a(m,n-1));
+	if(m==0)return n+1;
+	if(n==0)return a(m-1,1);
+	return a(m-1,a(m,n-1));
 }
 void main()
 {
